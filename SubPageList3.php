da4cbfb061c90354d233f74937e60d29db3a8650
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:57:16 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: Ib6ed259f20f932f58f2b26e1ec35e185f2c2b9db

--- SubPageList3.php
+++ SubPageList3.php
@@ -3,11 +3,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'SubPageList3' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['SubPageList3'] = __DIR__ . '/i18n';
-	/*wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for SubPageList3 extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);*/
+	);
 	return;
 } else {
 	die( 'This version of the SubPageList3 extension requires MediaWiki 1.25+' );
