2e35f9e21a86145468bfa7eb217a34f19d7dda06
Umherirrender <umherirrender_de.wp@web.de>
2023-09-16 19:33:21 +0200

    Use HookHandlers for core hook
    Bug: T346500
    Change-Id: I7bdfabf91f6a9b5eb48f50a27cdd5e82d2a1459e

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,14 +2,15 @@
 
 namespace MediaWiki\Extension\SubPageList3;
 
+use MediaWiki\Hook\ParserFirstCallInitHook;
 use Parser;
 
-class Hooks {
+class Hooks implements ParserFirstCallInitHook {
 
 	/**
 	 * @param Parser $parser
 	 */
-	public static function onParserFirstCallInit( Parser $parser ) {
+	public function onParserFirstCallInit( $parser ) {
 		$parser->setHook( 'splist', [ SubPageList3::class, 'renderSubpageList3' ] );
 	}
 }
